3ad827c85a620020e1bb5f78e2a72af8f6e1272f
DOXECEES <90423472+DOXECEES@users.noreply.github.com>
2025-05-20 14:00:23 +0300

    fix: depthbuffer now return depth texture

--- Engine/src/Renderer/OpenGL/DepthBuffer.hpp
+++ Engine/src/Renderer/OpenGL/DepthBuffer.hpp
@@ -1,32 +1,24 @@
 #ifndef SRC_RENDERER_OPENGL_DEPTHBUFFER_HPP
 #define SRC_RENDERER_OPENGL_DEPTHBUFFER_HPP
 
-#include "IBuffer.hpp"
+#include "FBO.hpp"
 
 namespace nb
 {
     namespace OpenGl
     {
-        class DepthBuffer : public IBuffer
+        class DepthBuffer : public FBO
         {
         public:
             DepthBuffer(const GLuint width = 1024, const GLuint height = 1024) noexcept;
             ~DepthBuffer() = default;
 
-            void bind() const noexcept override;
-            void unBind() const noexcept override;
-
             GLuint generateDepthMap(const GLuint width, const GLuint height) const noexcept;
 
             GLuint getDepthMap() const noexcept;
 
-            GLuint getWidth() const noexcept;
-            GLuint getHeight() const noexcept;
 
         private:
-            GLuint colorTexture = 0;
-            GLuint width        = 1024;
-            GLuint height       = 1024;
         };
     };
 };
