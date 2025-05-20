f6230fb9a1cf28074ed0e4b70e3cd1bf8845038d
DOXECEES <90423472+DOXECEES@users.noreply.github.com>
2025-05-20 15:11:28 +0300

    refactor: remove extractExtension method for std::string, fix the typos, other minor fixes

--- Engine/src/Manager/ResourceManager.cpp
+++ Engine/src/Manager/ResourceManager.cpp
@@ -6,10 +6,10 @@ namespace nb
     {
         ResourceManager::ResourceManager()
         {
-            regisrterLoader(".json", createRef<nb::Loaders::JsonFactory>());
-            regisrterLoader(".nbsd", createRef<nb::Loaders::JsonFactory>());
-            regisrterLoader(".shader", createRef<nb::Loaders::Factory::ShaderFactory>());
-            regisrterLoader(".obj", createRef<nb::Loaders::Factory::ObjFactory>());
+            registerLoader(".json", createRef<nb::Loaders::JsonFactory>());
+            registerLoader(".nbsd", createRef<nb::Loaders::JsonFactory>());
+            registerLoader(".shader", createRef<nb::Loaders::Factory::ShaderFactory>());
+            registerLoader(".obj", createRef<nb::Loaders::Factory::ObjFactory>());
         }
 
         ResourceManager *nb::ResMan::ResourceManager::getInstance() noexcept
@@ -18,7 +18,7 @@ namespace nb
             return &rm;
         }
 
-        void ResourceManager::regisrterLoader(std::string_view extention, Ref<nb::Loaders::Factory::IFactoryLoader> loader) noexcept
+        void ResourceManager::registerLoader(std::string_view extention, Ref<nb::Loaders::Factory::IFactoryLoader> loader) noexcept
         {
             loaders[extention.data()] = loader;
             createConcretePoolIfNotExists(loader->getResourceType());            
@@ -31,27 +31,33 @@ namespace nb
                 pool[resourceType] = ResourcePool();
             }
         }
-        std::string_view ResourceManager::extractExtention(std::string_view path) const noexcept
+        std::string_view ResourceManager::extractExtension(std::string_view path) const noexcept 
         {
-            return path.substr(path.find_last_of("."));
-        }
-
-        std::string ResourceManager::extractExtention(std::string path) const noexcept
-        {
-            return path.substr(path.find_last_of("."));
+            size_t dotPos = path.find_last_of('.');
+            if (dotPos == std::string_view::npos) return {};
+            return path.substr(dotPos);
         }
 
         void ResourceManager::load(const std::filesystem::path &path) noexcept
         {
-            std::string extention = path.extension().string();
-            if(loaders.find(extention.data()) == loaders.end()) 
+            std::string extension = path.extension().string();
+            auto loaderIterator = loaders.find(extension);
+            if (loaderIterator == loaders.end())
             {
-                Debug::debug("Loader not found");
+                Debug::debug("Loader not found for extension: " + extension);
                 abort();
             }
 
-            std::type_index type = loaders.at(extention.data())->getResourceType();   
-            pool[type][path.string()] = loaders.at(extention.data())->create(path);
+            std::type_index type = loaderIterator->second->getResourceType();   
+            pool[type][path.string()] = loaderIterator->second->create(path);
+        }
+
+        void ResourceManager::unload() noexcept
+        {
+            for(auto& concretePool : pool)
+            {
+                concretePool.second.clear();
+            }
         }
 
         bool ResourceManager::isAbsolutePath( std::string_view path) const noexcept
@@ -64,18 +70,23 @@ namespace nb
             return !isAbsolutePath(path);
         }
 
-        bool ResourceManager::isResourceLoaded(std::string_view path) const noexcept
+        bool ResourceManager::isResourceLoaded(std::string_view path) const noexcept 
         {
-            std::string_view extention = extractExtention(path);
-            if(loaders.find(extention.data()) == loaders.end()) 
+            std::string_view extension = extractExtension(path);
+
+            auto loaderIterator = loaders.find(extension.data());
+            if (loaderIterator == loaders.end())
             {
-                Debug::debug("Loader not found");
+                Debug::debug("Loader not found for extension: " + std::string(extension));
                 abort();
             }
 
-            std::type_index type = loaders.at(extention.data())->getResourceType();   
-            return pool.at(type).contains(path.data());        
+            std::type_index type = loaderIterator->second->getResourceType();
+            const auto& concretePool = pool.at(type);
+
+            return concretePool.contains(path.data());
         }
+
 
     };
 };
